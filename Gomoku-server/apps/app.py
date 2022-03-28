3d3802b9d85713604dd2c427ced12b05736a6f28
Jing Peng <qiqiai77a@126.com>
2022-03-28 13:05:08 -0400

    finish upload model api

--- Gomoku-server/apps/app.py
+++ Gomoku-server/apps/app.py
@@ -9,8 +9,7 @@ import sys
 import os
 sys.path.append("..")
 
-from apps.utils import run, PredictionCase
-from detectron2.data.datasets.builtin_meta import COCO_CATEGORIES
+from apps.utils import run 
 
 app = Flask(__name__)
 app_config = {"host": "127.0.0.1", "port": "5000"}
@@ -48,29 +47,16 @@ def upload_model():
     return jsonify(retdata)
 
 
-@app.route('/get', methods=['GET'])  # get image detetion results from model
+# TODO: API for human player
+@app.route('/get', methods=['GET'])  
 def getData():
-    url = request.args.get("url")  # image url uploaded by
-    # print(request.get_json())
-    # print(url)
-    # annos = []
-    # data = request.get_json()
+    url = request.args.get("url")  
     imgs = []
     imgs.append(url)
 
-    cases = run(imgs, model)
+    cases = run(imgs, url)
 
-    case_dict = {"url": url, "annotation": [], "height": 0, "width": 0}
-
-    for case in cases[0]:
-        anno = {}
-        anno["category"] = COCO_CATEGORIES[case.category]["name"]
-        anno["bbox"] = case.bbox
-        anno["confidence"] = case.confidence
-        case_dict["url"] = case.url
-        case_dict["annotation"].append(anno)
-        case_dict["height"] = case.height
-        case_dict["width"] = case.width
+    case_dict = {"url": url}
     print(case_dict)
 
     return jsonify(case_dict)
